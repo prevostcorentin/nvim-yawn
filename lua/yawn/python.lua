5e87e529c1427059f959607c502bebfbb76908c1
Corentin Prevost <prevost.corentin@yahoo.com>
2023-01-24 15:44:34 +0100

    [-] (#3) Refactoring using plenary and built-in NeoVim Lua functions

--- lua/yawn/python.lua
+++ lua/yawn/python.lua
@@ -3,6 +3,8 @@ local yawn = {
 	filesystem = require("yawn.filesystem"),
 }
 
+local Path = require("plenary.path")
+
 local internal = {
 	constants = {
 		["DEFAULT_VENV_NAME"] = "venv",
@@ -14,90 +16,25 @@ local internal = {
 local M = {}
 
 function M.has_venv()
-	local has_venv = internal._has_user_venv()
-	if has_venv then
-		return has_venv
-	end
-	has_venv = internal._has_default_venv()
-	return has_venv
-end
-
-function M.find_interpreter()
-	local interpreter = nil
-	local is_user_venv = internal._has_user_venv()
-	local is_default_venv = internal._has_default_venv()
-	if is_user_venv then
-		interpreter = internal._find_user_venv_interpreter()
-	elseif is_default_venv then
-		interpreter = internal._find_default_venv_interpreter()
-	else
-		interpreter = internal._find_system_interpreter()
-	end
-	return interpreter
+	local venv_directory = internal._find_venv_directory()
+	local supposed_activate_script_path = Path.new(venv_directory, internal.constants.ACTIVATE_SCRIPT_PATH)
+	local has_activate_script_existing = supposed_activate_script_path:is_file()
+	return has_activate_script_existing
 end
 
 function M.find_venv()
-	local is_user_venv = internal._has_user_venv()
-	local is_default_venv = internal._has_default_venv()
-	local venv_name = nil
-	if is_user_venv then
-		venv_name = yawn.workspace.get("python.venv.name")
-	elseif is_default_venv then
-		venv_name = internal.constants.DEFAULT_VENV_NAME
-	else
-		assert(false, "No virtual environment defined")
+	if not M.has_venv() then
+		assert(false, "No virtual environment")
 	end
-	local venv_directory = yawn.filesystem.join_paths({ yawn.workspace.get("directory"), venv_name })
+	local venv_directory = internal._find_venv_directory()
 	return venv_directory
 end
 
-function internal._has_user_venv()
-	local has_user_defined_venv = yawn.workspace.defines("python.venv")
-	return has_user_defined_venv
-end
-
-function internal._has_default_venv()
-	local default_venv_directory = yawn.filesystem.join_paths({ yawn.workspace.get("directory"), "venv" })
-	local activate_script_path =
-		yawn.filesystem.join_paths({ default_venv_directory, internal.constants.ACTIVATE_SCRIPT_PATH })
-	local is_existing_directory = yawn.filesystem.is_directory(default_venv_directory)
-	local has_activate_script = yawn.filesystem.is_file(activate_script_path)
-	local has_default_venv = false
-	if is_existing_directory and has_activate_script then
-		has_default_venv = true
-	end
-	return has_default_venv
-end
-
-function internal._find_user_venv_interpreter()
-	local interpreter_name = yawn.workspace.get("python.interpreter", "python")
-	local interpreter_path = yawn.filesystem.join_paths({
-		yawn.workspace.get("directory"),
-		yawn.workspace.get("python.venv.name"),
-		"bin",
-		interpreter_name,
-	})
-	return interpreter_path
-end
-
-function internal._find_default_venv_interpreter()
-	local interpreter_name = yawn.workspace.get("python.interpreter", "python")
-	local default_venv_interpreter_path = yawn.filesystem.join_paths({
-		yawn.workspace.get("directory"),
-		internal.constants.DEFAULT_VENV_NAME,
-		"bin",
-		interpreter_name,
-	})
-	local is_existing_interpreter = yawn.filesystem.is_file(default_venv_interpreter_path)
-	if not is_existing_interpreter then
-		assert(false, default_venv_interpreter_path .. " is not a Python interpreter")
-	end
-	return default_venv_interpreter_path
-end
-
-function internal._find_system_interpreter()
-	local interpreter = yawn.workspace.get("python.interpreter", "python")
-	return interpreter
+function internal._find_venv_directory()
+	local workspace_directory = yawn.workspace.get("directory")
+	local venv_name = yawn.workspace.get("python.venv", internal.constants.DEFAULT_VENV_NAME)
+	local venv_directory = Path.new(workspace_directory, venv_name, internal.constants.ACTIVATE_SCRIPT_PATH)
+	return venv_directory
 end
 
 return M
--- lua/yawn/workspace.lua
+++ lua/yawn/workspace.lua
@@ -1,4 +1,4 @@
-local filesystem = require("yawn.filesystem")
+local Path = require("plenary.path")
 
 local internal = {
 	constants = {
@@ -12,21 +12,24 @@ local M = {}
 function M.get(option, default)
 	local workspace = M.load()
 	local value = workspace
-	if M.defines(option) then
-		for option_part in internal._split_option(option) do
+	local is_option_defined = M.defines(option)
+	if not is_option_defined then
+		if default then
+			value = default
+		else
+			assert(
+				false,
+				option
+					.. " is not defined in "
+					.. internal.constants.MODULE_DIRECTORY
+					.. "/"
+					.. internal.constants.MAIN_FILE_NAME
+			)
+		end
+	elseif is_option_defined then
+		for option_part in vim.fn.split(option, "\\.") do
 			value = value[option_part]
 		end
-	elseif nil ~= default then
-		value = default
-	else
-		assert(
-			false,
-			option
-				.. " is not defined in "
-				.. internal.constants.MODULE_DIRECTORY
-				.. "/"
-				.. internal.constants.MAIN_FILE_NAME
-		)
 	end
 	return value
 end
@@ -34,7 +37,7 @@ end
 function M.defines(option)
 	local workspace_definition = M.load()
 	local is_defining_option = true
-	for option_part in internal._split_option(option) do
+	for option_part in vim.fn.split(option, "\\.") do
 		workspace_definition = workspace_definition[option_part]
 		if nil == workspace_definition then
 			is_defining_option = false
@@ -44,35 +47,17 @@ function M.defines(option)
 	return is_defining_option
 end
 
-function internal._split_option(option)
-	local splitted_options_iterator = string.gmatch(option, "%a+")
-	return splitted_options_iterator
-end
-
 function M.load()
-	local settings_file_path = internal._build_settings_file_path()
-	local local_settings_module = internal._load_settings_file(settings_file_path)
-	if nil == local_settings_module then
-		local_settings_module = {}
+	local workspace_directory = vim.fn.getcwd()
+	local workspace_module_file_path =
+		Path.new(workspace_directory, internal.constants.MODULE_DIRECTORY, internal.constants.MAIN_FILE_NAME)
+	local require_workspace_module = assert(loadfile(workspace_module_file_path.filename))
+	local workspace_module = {}
+	if workspace_module_file_path:is_file() then
+		workspace_module = require_workspace_module()
 	end
-	local_settings_module.directory = filesystem.get_current_working_directory()
-	return local_settings_module
-end
-
-function internal._build_settings_file_path()
-	local current_working_directory = filesystem.get_current_working_directory()
-	local module_directory = current_working_directory .. "/" .. internal.constants.MODULE_DIRECTORY
-	local settings_file_path = module_directory .. "/" .. internal.constants.MAIN_FILE_NAME
-	return settings_file_path
-end
-
-function internal._load_settings_file(settings_file_path)
-	local settings_module = nil
-	if filesystem.is_file(settings_file_path) then
-		local settings_module_require = assert(loadfile(settings_file_path))
-		settings_module = settings_module_require()
-	end
-	return settings_module
+	workspace_module.directory = vim.fn.getcwd()
+	return workspace_module
 end
 
 return M
